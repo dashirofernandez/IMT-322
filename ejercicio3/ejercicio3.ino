57fe9446e84eb10a9593299c7d8064cfc4c63d57
dashirofernandez <dagiromotors12@gmail.com>
2024-03-09 12:09:32 -0400

    ejercicio3punto1

---
+++ ejercicio3/ejercicio3.ino
@@ -0,0 +1,29 @@
+#define PIN_PULSADOR_1 5
+#define PIN_PULSADOR_2 18
+
+
+// Variable para almacenar el contador
+volatile int contador = 0;
+void incrementarContador() {
+  contador++;
+}
+
+void reiniciarContador() {
+  contador = 0;
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(PIN_PULSADOR_1, INPUT_PULLUP);
+  pinMode(PIN_PULSADOR_2, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(PIN_PULSADOR_1), incrementarContador, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PIN_PULSADOR_2), reiniciarContador, FALLING);
+}
+
+void loop() {
+
+  Serial.println(contador);
+
+  delay(2000);
+}
