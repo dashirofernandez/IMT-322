247c15093cd33d26bd109022c1ddc06f5598cc83
dashirofernandez <dagiromotors12@gmail.com>
2024-04-23 09:55:53 -0400

    practica ascensor

---
+++ ascensor/ascensor.ino
@@ -0,0 +1,43 @@
+int led1 = 4;
+int led2 = 5;
+int led3 = 6;
+char letra;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available()) {
+    char inChar = (char)Serial.read();
+    letra = inChar; 
+    switch (letra) {
+      case 'a':
+        digitalWrite(led1, HIGH);
+        digitalWrite(led2, LOW);
+        digitalWrite(led3, LOW);
+      	Serial.println("En camino al piso 1");
+        break;
+      case 'b':
+        digitalWrite(led1, LOW);
+        digitalWrite(led2, HIGH);
+        digitalWrite(led3, LOW);
+      	Serial.println("En camino al piso 2");
+        break;
+      case 'c':
+        digitalWrite(led1, LOW);
+        digitalWrite(led2, LOW);
+        digitalWrite(led3, HIGH);
+      	Serial.println("En camino al piso 3");
+        break;
+      default:
+        digitalWrite(led1, LOW);
+        digitalWrite(led2, LOW);
+        digitalWrite(led3, LOW);
+        break;
+    }
+  }
+}
