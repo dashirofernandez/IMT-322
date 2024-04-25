f5b42d7145f1bece28160017eaca8c61911aa9ab
dashirofernandez <dagiromotors12@gmail.com>
2024-04-24 23:02:22 -0400

    ascensor

--- ascensor/ascensor.ino
+++ ascensor/ascensor.ino
@@ -20,18 +20,39 @@ void loop() {
         digitalWrite(led2, LOW);
         digitalWrite(led3, LOW);
       	Serial.println("En camino al piso 1");
+      	for (int i = 0; i <= 100; i++) {
+    		Serial.print("\r");
+    		Serial.print("Cargando: ");
+    		Serial.print(i);
+    		Serial.print("%");
+    		delay(100); 
+  		}
         break;
       case 'b':
         digitalWrite(led1, LOW);
         digitalWrite(led2, HIGH);
         digitalWrite(led3, LOW);
       	Serial.println("En camino al piso 2");
+      	for (int i = 0; i <= 100; i++) {
+    		Serial.print("\r");
+    		Serial.print("Cargando: ");
+    		Serial.print(i);
+    		Serial.print("%");
+    		delay(100); 
+  		}
         break;
       case 'c':
         digitalWrite(led1, LOW);
         digitalWrite(led2, LOW);
         digitalWrite(led3, HIGH);
       	Serial.println("En camino al piso 3");
+      	for (int i = 0; i <= 100; i++) {
+    		Serial.print("\r");
+    		Serial.print("Cargando: ");
+    		Serial.print(i);
+    		Serial.print("%");
+    		delay(100); 
+  		}
         break;
       default:
         digitalWrite(led1, LOW);
